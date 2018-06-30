15021047d8ad078cf850997a2396531459d49353
Mikaela Szekely <qyriad@gmail.com>
2018-06-30 07:53:02 -0600

    Fix #20: Typo that still actually worked
    `switch_to_highbuf()` mistakenly refers to `switch` instead of `self`.
    Though, it doesn't actually matter since in this particular script they're the
    same object.

--- fusee-launcher.py
+++ fusee-launcher.py
@@ -546,8 +546,8 @@ class RCMHax:
     def switch_to_highbuf(self):
         """ Switches to the higher RCM buffer, reducing the amount that needs to be copied. """
 
-        if switch.get_current_buffer_address() != self.COPY_BUFFER_ADDRESSES[1]:
-            switch.write(b'\0' * 0x1000)
+        if self.get_current_buffer_address() != self.COPY_BUFFER_ADDRESSES[1]:
+            self.write(b'\0' * 0x1000)
 
 
     def trigger_controlled_memcpy(self, length=None):
